0ab6448b88f6b4520eba61908f38b0a6a4b5d6f3
kleeeeea <klee@cs.ucsb.edu>
2021-09-19 00:58:46 -0400

    backup

--- Wikit/eventPage.js
+++ Wikit/eventPage.js
@@ -10,10 +10,9 @@ function fixedEncodeURI(str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
-function search_youtube(text, clickData) {
+function search_youtube(text) {
     var wikiUrl = "https://www.youtube.com/results?search_query=" + fixedEncodeURI(text);
     // "https://en.wikipedia.org/wiki/"
-    console.log(clickData.selectionText);
     console.log('clickData.selectionText');
     // var createData = {
     //     "url": wikiUrl,
@@ -34,30 +33,50 @@ function search_youtube(text, clickData) {
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
     if (clickData.menuItemId == "Wikit" && clickData.selectionText) {
+        console.log(clickData.selectionText);
 
         let text = clickData.selectionText;
-        search_youtube(text, clickData);
+
+        let data = {element: "barium"};
+
+        fetch("http://localhost:5000/mlx", {
+            method: "POST",
+            body: JSON.stringify(data)
+        }).then(res => {
+            res.json().then(
+                data => {
+                    console.log("Request complete! response:", res);
+                }
+            )
+        });
+        // search_youtube(text);
     }
 
 });
 
 
 chrome.commands.onCommand.addListener(function (command) {
-    switch (command) {
-        case 'duplicate-tab':
-            duplicateTab();
-            break;
-        default:
-            console.log(`Command ${command} not found`);
+    if (command.startsWith('search')) {
+
+        chrome.tabs.executeScript({
+            code: "window.getSelection().toString();"
+        }, function (selection) {
+            text = selection[0];
+            search_youtube(text);
+        });
+        //https://dev.to/paulasantamaria/adding-shortcuts-to-your-chrome-extension-2i20#:~:text=The%20user%20can%20bind%20the,chrome%3A%2F%2Fextensions%2Fshortcuts%20).&text=Any%20keyboard%20shortcut%20must%20use,We%20can%20also%20use%20Shift%20.
+        // duplicateTab();
+    } else {
+        console.log(`Command ${command} not found`);
     }
 });
 
 /**
-* Gets the current active tab URL and opens a new tab with the same URL.
-*/
+ * Gets the current active tab URL and opens a new tab with the same URL.
+ */
 function duplicateTab() {
-    const query = { active: true, currentWindow: true };
+    const query = {active: true, currentWindow: true};
     chrome.tabs.query(query, (tabs) => {
-        chrome.tabs.create({ url: tabs[0].url, active: false });
+        chrome.tabs.create({url: tabs[0].url, active: false});
     });
 }
